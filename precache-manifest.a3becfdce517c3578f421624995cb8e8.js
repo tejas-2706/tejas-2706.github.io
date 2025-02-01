b777e298844024faf74f30543336802232ceae47
tejas <pangaonkar.svc424@gmail.com>
2025-02-01 22:51:44 +0530

    Updates

---
+++ precache-manifest.a3becfdce517c3578f421624995cb8e8.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c6d558cc45a697f64b04d88a2351be6",
+    "url": "/tejas-2706/tejas-2706.github.io/index.html"
+  },
+  {
+    "revision": "880a9244836e8cbc3460",
+    "url": "/tejas-2706/tejas-2706.github.io/static/css/2.c434d6cc.chunk.css"
+  },
+  {
+    "revision": "90a45828af26115cf89e",
+    "url": "/tejas-2706/tejas-2706.github.io/static/css/main.a6e94e8a.chunk.css"
+  },
+  {
+    "revision": "880a9244836e8cbc3460",
+    "url": "/tejas-2706/tejas-2706.github.io/static/js/2.23302d0e.chunk.js"
+  },
+  {
+    "revision": "bc85b45e484910a02fc7",
+    "url": "/tejas-2706/tejas-2706.github.io/static/js/3.4806d335.chunk.js"
+  },
+  {
+    "revision": "90a45828af26115cf89e",
+    "url": "/tejas-2706/tejas-2706.github.io/static/js/main.39cdd4aa.chunk.js"
+  },
+  {
+    "revision": "fe87846f2f0782f49e52",
+    "url": "/tejas-2706/tejas-2706.github.io/static/js/runtime~main.16129ba7.js"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "f750170aaddf4e5f885a27d391a311d9",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/arrow1.f750170a.svg"
+  },
+  {
+    "revision": "9070490babe4d1aa7f077b104555b363",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/arrow2.9070490b.svg"
+  },
+  {
+    "revision": "5f3ca49706055342e92708c7dd31849f",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/banner-bg.5f3ca497.png"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/color-sharp.d6409477.png"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/contact-img.a8822e20.svg"
+  },
+  {
+    "revision": "e9228c17c13a87653f1b8ed01a3ff306",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/footer-bg.e9228c17.png"
+  },
+  {
+    "revision": "bfe6d1a6d579d73c0c625ec0afe2855b",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/header-img.bfe6d1a6.svg"
+  },
+  {
+    "revision": "30b1b0f8ae22023e070eb0a3e447e7b0",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/logo.30b1b0f8.svg"
+  },
+  {
+    "revision": "dea05c56b45857a24dcba5a23202ec70",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/meter1.dea05c56.svg"
+  },
+  {
+    "revision": "7fb1b05f49ba5c3b4570e1dec3728213",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/meter2.7fb1b05f.svg"
+  },
+  {
+    "revision": "19e4204daee7b06ccd0aa7807bc37653",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/meter3.19e4204d.svg"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "abfa9a9a7ed934c39d517265452539fd",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/nav-icon2.abfa9a9a.svg"
+  },
+  {
+    "revision": "81ff4e970a39053a0acdc10fd58dc5c4",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/nav-icon3.81ff4e97.svg"
+  },
+  {
+    "revision": "964b4f97e086e7592d4499f06a701a80",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/project-img1.964b4f97.png"
+  },
+  {
+    "revision": "30d51f397c718d11ec8c56eb45b21426",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/project-img2.30d51f39.png"
+  },
+  {
+    "revision": "73369ad112a412e20bb7371d12a1b5ff",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/project-img3.73369ad1.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/tejas-2706/tejas-2706.github.io/static/media/revicons.2feb69cc.eot"
+  }
+]);
